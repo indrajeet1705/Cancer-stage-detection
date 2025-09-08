54c697a8df6f2cd25fd1c92f3596295725b7c9ac
indrajeet bhujabl <indrajeetbhujbal0@gmail.com>
2025-09-08 22:49:36 +0530

    final commit

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
 from transformers import AutoModelForCausalLM, AutoTokenizer,pipeline
-import torch
+
+import os
 from  flask_cors import CORS 
 
 # Load cancer stage prediction model and encoders
@@ -66,4 +67,5 @@ def predict():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
